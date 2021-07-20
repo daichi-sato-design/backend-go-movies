3c1561daae57f95117698f8d6562bf1ada556dd2
daichi-sato-design <daichisato.design@gmail.com>
2021-07-20 23:24:15 +0900

    app関数：映画前取得実装(getAllMovie)

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -18,18 +18,14 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	app.logger.Println("id id", id)
-
 	movie, err := app.models.DB.Get(id)
 	if err != nil{
-		app.logger.Print(err)
 		app.ErrorJSON(w, err)
 		return
 	}
 
 	err = app.WriteJSON(w, http.StatusOK, movie, "movie")
 	if err != nil{
-		app.logger.Print(err)
 		app.ErrorJSON(w, err)
 		return
 	}
@@ -37,5 +33,31 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request){
+	movies, err := app.models.DB.All()
+	if err != nil{
+		app.ErrorJSON(w, err)
+		return
+	}
+
+	err = app.WriteJSON(w, http.StatusOK, movies, "movies")
+	if err != nil{
+		app.ErrorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request){
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request){
 	
 }
--- models/movies-db.go
+++ models/movies-db.go
@@ -67,11 +67,77 @@ func (m *DBModel) Get(id int) (*Movie, error){
 
 	movie.MovieGenre = genres
 
-
 	return &movie, nil
 }
 
 // All returns all movies and error, if any
-func (m *DBModel) All(id int) ([]*Movie, error){
-	return nil, nil
+func (m *DBModel) All() ([]*Movie, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel();
+
+	query := `select id, title, description, year, release_date, runtime, rating, mpaa_rating,
+				created_at, updated_at from movies order by title
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var movies []*Movie
+
+	for rows.Next(){
+		var movie Movie
+		err := rows.Scan(
+			&movie.ID,
+			&movie.Title,
+			&movie.Description,
+			&movie.Year,
+			&movie.ReleaseDate,
+			&movie.Runtime,
+			&movie.Rating,
+			&movie.MPAARating,
+			&movie.CreatedAd,
+			&movie.UpdatedAt,	
+		)
+		if err != nil{
+			return nil, err
+		}
+
+		// get the genres
+		genreQuery := `select
+					mg.id, mg.movie_id, mg.genre_id, g.genre_name
+				from
+					movies_genres mg
+					left join genres g on (g.id = mg.genre_id)
+				where
+					mg.movie_id = $1 
+	
+		`
+		genreRows, _ := m.DB.QueryContext(ctx, genreQuery, movie.ID)
+	
+		genres := make(map[int]string)
+		for genreRows.Next(){
+			var mg MovieGenre
+			err := genreRows.Scan(
+				&mg.ID,
+				&mg.MovieID,
+				&mg.GenreID,
+				&mg.Genre.GenreName,
+			)
+			if err != nil{
+				return nil, err
+			}
+			genres[mg.ID] = mg.Genre.GenreName
+		}
+
+		genreRows.Close()
+
+		movie.MovieGenre = genres
+		movies = append(movies, &movie)
+	
+	}
+	return movies, nil
+
 }
