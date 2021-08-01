ff6e0694390a197364180def4b53c25fa5988901
daichi-sato-design <daichisato.design@gmail.com>
2021-08-01 15:16:48 +0900

    checkTokenのミドルウェア実装

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,10 +1,75 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) checkToken(next http.Handler)http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Vary", "Authorization")
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			// could set an anonymous user
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2{
+			app.errorJSON(w, errors.New("invalid auth header"))
+			return
+		}
+
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(w, errors.New("unauthorizad - no bearer"))
+			return
+		}
+
+		token := headerParts[1]
+
+		cliams, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+		if err != nil {
+			app.errorJSON(w, errors.New("unauthorizad - failed hmac check"))
+			return
+		}
+
+		if !cliams.Valid(time.Now()){
+			app.errorJSON(w, errors.New("unauthorizad - token expired"))
+			return
+		}
+
+		if !cliams.AcceptAudience("mydomain.com"){
+			app.errorJSON(w, errors.New("unauthorizad - invalid audience"))
+			return
+		}
+
+		if cliams.Issuer != "mydomain.com"{
+			app.errorJSON(w, errors.New("unauthorizad - invalid issuer"))
+			return
+		}
+
+		userID, err := strconv.ParseInt(cliams.Subject, 10, 64)
+		if err != nil{
+			app.errorJSON(w, errors.New("unauthorizad"))
+			return
+		}
+
+		log.Println("Valid user: ", userID)
 
 		next.ServeHTTP(w, r)
 	})
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 	github.com/lib/pq v1.10.0 // indirect
 	github.com/pascaldekloe/jwt v1.10.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
